fc5f3df24108f616148aff4361b5764b33a03b7d
imrasp <games.rasp@gmail.com>
2017-11-21 17:18:19 +0700

    update

--- script/server_build.sh
+++ script/server_build.sh
@@ -4,4 +4,4 @@ rm -rf  /home/rasp/CLionProjects/LearnVI_Drone/server_build
 mkdir  /home/rasp/CLionProjects/LearnVI_Drone/server_build
 rsync -a --exclude 'LearnVI_Drone/sample_data' --exclude 'LearnVI_Drone/build' --exclude 'LearnVI_Drone/dowloaded_result' --exclude 'LearnVI_Drone/Performance_output_result' --exclude 'LearnVI_Drone/Vocabulary' /home/rasp/CLionProjects/LearnVI_Drone vgl-gpu1:/home/teera/workspace/rasp/
 ssh vgl-gpu1 '/home/teera/workspace/rasp/run_docker_without_gui.sh'
-rsync -av vgl-gpu1:/home/teera/workspace/rasp/LearnVI_Drone/build/. /home/rasp/CLionProjects/LearnVI_Drone/server_build/
+rsync -a vgl-gpu1:/home/teera/workspace/rasp/LearnVI_Drone/build/. /home/rasp/CLionProjects/LearnVI_Drone/server_build/
