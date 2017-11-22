488e21a653521f978d63e1af65aa40039774a709
imrasp <games.rasp@gmail.com>
2017-11-22 13:35:20 +0700

    update

---
+++ script/run_odroid_crosscompile.sh
@@ -0,0 +1,2 @@
+rm -rf /home/odroid/workspace/VIDrone/sample_data/datasetname
+/home/odroid/workspace/VIDrone/xLearVI_Drone -m "LIVERECORD" -f "datasetname" -t 100000 -b 921600 -d "/dev/ttySAC0" -g "DISABLE"
