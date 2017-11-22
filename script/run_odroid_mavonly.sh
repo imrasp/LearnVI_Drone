153cea21856ebebf6ad0c6174cad082e6ca12f0c
imrasp <games.rasp@gmail.com>
2017-11-22 20:54:04 +0700

    update

--- script/run_odroid_mavonly.sh
+++ script/run_odroid_mavonly.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 rm -rf /home/odroid/workspace/VIDrone/sample_data/datasetname
-/home/odroid/workspace/VIDrone/build/LearnVI_Drone -m "MAVONLY" -f "datasetname" -t 100000 -b 921600 -d "/dev/ttyUSB0" -g "DISABLE"
+/home/odroid/workspace/VIDrone/build/LearnVI_Drone -m "MAVONLY" -f "datasetname" -t 100000 -b 921600 -d "/dev/ttySAC0" -g "DISABLE"
+
