e94fe3e46c3b5c5e10134f6a2d02d5379deb4340
imrasp <games.rasp@gmail.com>
2017-11-30 11:28:39 +0700

    update

---
+++ script/run_new.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf ../sample_data/datasetname
+./../server_build/LearnVI_Drone -c "/home/rasp/CLionProjects/LearnVI_Drone/config.system.yaml"
+
--- script/run_odroid_new.sh
+++ script/run_odroid_new.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -rf /home/odroid/workspace/VIDrone/sample_data/datasetname
-/home/odroid/workspace/VIDrone/build/LearnVI_Drone -m "LIVERECORD" -c "/home/odroid/workspace/VIDrone/config/system.yaml"
+/home/odroid/workspace/VIDrone/build/LearnVI_Drone -c "/home/odroid/workspace/VIDrone/config/system_odroid.yaml"
