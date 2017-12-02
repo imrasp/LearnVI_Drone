084428d6479a208f8d07186cfc23d36b8d451949
imrasp <games.rasp@gmail.com>
2017-12-02 15:30:07 +0700

    update

---
+++ script/run_odroid_new_mavonly.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf /home/odroid/workspace/VIDrone/sample_data/datasetname
+/home/odroid/workspace/VIDrone/build/LearnVI_Drone -c "/home/odroid/workspace/VIDrone/config/system_odroid2.yaml"
